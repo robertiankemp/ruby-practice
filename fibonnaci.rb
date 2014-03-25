98f136f48a7db74709294d48c7d1324008d4f4e9
MalPinder <rob.kemp@fundingcircle.com>
2014-03-25 12:34:20 +0000

    fibbo_sequence

--- fibonnaci.rb
+++ fibonnaci.rb
@@ -1,9 +1,10 @@
-def fibonnaci_sequence
-
-  (1..10).each { |n|
-   array = [0, 1]
-       
-   
+def fibonnaci_sequence(number)
+  @fibbo = [1, 1]
+  (1..number).each { |n|
+    break if @fibbo.length == number
+  @fibbo << @fibbo[n-1] + @fibbo[n]
+  }
+  puts @fibbo
 end
 
-fibonnaci_sequence
+fibonnaci_sequence(500)
--- square_of_sum.rb
+++ square_of_sum.rb
@@ -1,22 +1,26 @@
-def sum_of_squares
-array = []
-(1..3).each { |n|
-  array << n**2
-}
+puts "Give me a number and I will produce the difference between
+the square of sums and the sum of squares."
 
-puts array
-sum_of_squares_total = array.inject(:+)
-puts sum_of_squares_total
+@no = gets.chomp.to_i
+@numbers = (1..@no)
 
-end
+@sum_of_squares_total = @numbers.map { |n| n ** 2}.inject(:+)
 
-sum_of_squares
+total_no2 = @numbers.to_a.inject(:+)       
+@square_of_sum_total = total_no2**2
 
-def square_of_sum
+  def total
+    puts "your sum of squares is #{@sum_of_squares_total}"
+    puts "and your square of sums is #{@square_of_sum_total}"
+    difference = @square_of_sum_total - @sum_of_squares_total
+    puts "and the difference is #{difference}"
+  end
+
+total
 
 
 
 
-end
+
 
 
